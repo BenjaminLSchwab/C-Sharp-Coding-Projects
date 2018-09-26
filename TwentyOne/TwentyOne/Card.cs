bed61305272800999bde82131a8b9846c437a9c1
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-26 15:47:06 -0700

    cards, decks, and shuffling of decks

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card(string face, string suit)
+        {
+            Suit = suit;
+            Face = face;
+        }
+
+        public string Display()
+        {
+            return Face + " of " + Suit;
+        }
+
+        public string Suit { get; set; }
+        public string Face { get; set; }
+
+    }
+}
---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+   public class Deck
+    {
+        public List<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            string[] suits = {"Spades", "Hearts", "Diamonds", "Clubs" };
+            string[] faces = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Jack" };
+
+            Cards = new List<Card>();
+
+            foreach (string suit in suits)
+            {
+                foreach (string face in faces)
+                {
+                    Card card = new Card(face, suit);
+                    Cards.Add(card);
+                }
+            }         
+
+        }
+    }
+}
