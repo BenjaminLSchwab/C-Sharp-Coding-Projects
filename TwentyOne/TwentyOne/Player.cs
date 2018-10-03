6d9195fae65b7853dcd3f883a562f1fa42cb1ed4
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-03 11:48:35 -0700

    Through step 142

--- TwentyOne/TwentyOne/Player.cs
+++ TwentyOne/TwentyOne/Player.cs
@@ -19,6 +19,7 @@ namespace TwentyOne
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool IsActivleyPlaying { get; set; }
+        public bool Stay { get; set; }
 
         public static Game operator + (Game game, Player player)
         {
