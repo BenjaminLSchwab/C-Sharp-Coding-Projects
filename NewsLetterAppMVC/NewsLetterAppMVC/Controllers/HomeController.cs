eb13b8e56c3c41da0e79df0d4285da7a8189f3b4
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-10 13:08:44 -0700

    Removed commented code and unused connection string

--- NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -12,9 +12,6 @@ namespace NewsLetterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
-                                        Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;
-                                        Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public ActionResult Index()
         {
@@ -39,25 +36,6 @@ namespace NewsLetterAppMVC.Controllers
                 db.SignUps.Add(signUp);
                 db.SaveChanges();
             }
-                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                //                        (@FirstName, @LastName, @EmailAddress)";
-                //using (SqlConnection connection = new SqlConnection(connectionString))
-                //{
-                //    SqlCommand command = new SqlCommand(queryString, connection);
-                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-
-                //    command.Parameters["@FirstName"].Value = firstName;
-                //    command.Parameters["@LastName"].Value = lastName;
-                //    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                //    connection.Open();
-                //    command.ExecuteNonQuery();
-                //    connection.Close();
-
-                //}
-
                 return View("Success");
         }
 
@@ -78,30 +56,6 @@ namespace NewsLetterAppMVC.Controllers
 
                 return View(SignUpVms);
             }
-                //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
-                //List<NewsletterSignUp> signUps = new List<NewsletterSignUp>();
-
-                //using (SqlConnection connection = new SqlConnection(connectionString))
-                //{
-                //    SqlCommand command = new SqlCommand(queryString, connection);
-
-                //    connection.Open();
-
-                //    SqlDataReader reader = command.ExecuteReader();
-                //    while (reader.Read())
-                //    {
-                //        NewsletterSignUp signUp = new NewsletterSignUp();
-                //        signUp.Id = Convert.ToInt32(reader["Id"]);
-                //        signUp.FirstName = reader["FirstName"].ToString();
-                //        signUp.LastName = reader["LastName"].ToString();
-                //        signUp.EmailAddress = reader["EmailAddress"].ToString();
-                //        signUp.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-                //        signUps.Add(signUp);
-                //    }
-
-                //    //connection.Close();
-                //}
-
         }
     }
 }
